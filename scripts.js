85dd9968ade57f84fe7f468c5c63da3844d235a2
Faheem <faheemmbasheer@gmail.com>
2024-09-25 10:58:22 +0530

    create buttons and functions for calculation

---
+++ scripts.js
@@ -0,0 +1,44 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(operator, x, y) {
+    switch(operator) {
+        case "+":
+            add(x, y);
+        case "-":
+            subtract(x, y);
+        case "*":
+            multiply(x, y);
+        case "/":
+            divide(x, y);
+    }
+}
+
+// Creating the buttons inside .container
+const buttonList = [[1, 2, 3, "+"], [4, 5, 6, "-"], [7, 8, 9, "*"], [0, ".", "=", "/"]];
+const container = document.querySelector(".container");
+for (buttons of buttonList) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    container.appendChild(row);
+
+    for (buttonItem of buttons) {
+        const button = document.createElement("button");
+        button.textContent = buttonItem;
+        
+        row.appendChild(button);
+    }
+}
