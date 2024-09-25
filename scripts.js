71fcfd23f689430886807e44610230a188e17baf
Faheem <faheemmbasheer@gmail.com>
2024-09-25 18:11:41 +0530

    implement the logic to calculate properly

--- scripts.js
+++ scripts.js
@@ -17,13 +17,13 @@ function divide(x, y) {
 function operate(operator, x, y) {
     switch(operator) {
         case "+":
-            add(x, y);
+            return add(x, y);
         case "-":
-            subtract(x, y);
+            return subtract(x, y);
         case "*":
-            multiply(x, y);
+            return multiply(x, y);
         case "/":
-            divide(x, y);
+            return divide(x, y);
     }
 }
 
@@ -36,9 +36,13 @@ const row = document.createElement("div");
 row.setAttribute("class", "row");
 container.appendChild(row);
 const display = document.createElement("label");
-// display.textContent = "124";
 row.appendChild(display);
 
+
+// global variables
+let operator = null;
+let firstNum = null;
+let operatorPressed = false;
 // Adding the buttons
 for (buttons of buttonList) {
     const row = document.createElement("div");
@@ -50,8 +54,12 @@ for (buttons of buttonList) {
         button.textContent = buttonItem;
         if ("+-*/".includes(buttonItem)) {
             button.setAttribute("class", "btn operator");
-        } else if (!"=Clear".includes(buttonItem)) {
+            // Event listener to update the global "operator" variable on click
+            button.addEventListener("click", operatorFunction);
+        } else if (!"Clear=".includes(buttonItem)) {
             button.setAttribute("class", "btn");
+        } else {
+            button.setAttribute("class", "special");
         }
         button.setAttribute("id", buttonItem);
         
@@ -59,14 +67,47 @@ for (buttons of buttonList) {
     }
 }
 
+function operatorFunction(event) {
+    // Not doing calculation if the last button pressed was an operator
+    if (!operatorPressed) {
+        // if firstNum != null --> an operator was clicked before this operator click
+        // then firstNumber = operate(operator, firstNumber, secondNumber)
+        // secondNumber = +display.textContent
+        if (firstNum !== null) {
+            firstNum = operate(operator, firstNum, +display.textContent);
+            display.textContent = firstNum;
+        } else {
+            firstNum = +display.textContent;
+        }
+    }
+
+    // Keeping the operator the same if "=" was pressed
+    if (!(event.target.id === "=")) {
+        operator = event.target.id;
+    }
+    operatorPressed = true;
+}
+
 // Logic to make the buttons text appear inside display
 // (making use of event delegation here)
 container.addEventListener("click", (event) => {
     const buttonValue = event.target.attributes.class.value;
     if (buttonValue == "btn") {
+        // clearing the display if an operator was pressed before this click
+        if (operatorPressed) {
+            display.textContent = "";
+            operatorPressed = false;
+        }
         display.textContent += event.target.textContent;
-    } else if (buttonValue == "btn operator") { // Adding spaces around operators like so "x + y"
-        display.textContent += ` ${event.target.textContent} `;
+    }
+});
+
+
+const equal = document.getElementById("=");
+equal.addEventListener("click", (event) => {
+    // ignoring the click if an operator was not clicked before this
+    if (operator) {
+        operatorFunction(event);
     }
 });
 
